ed21b0bb5f7cff18aa021a8898daabdcac6cfd14
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-05-31 23:03:35 +0530

    Added redis queue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import connectDB from './config/dbConfig.js';
-import mailer from './config/mailConfig.js'
+//import mailer from './config/mailConfig.js';
 import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
 
@@ -10,9 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api', apiRouter);
-
 app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
