80a0601eed4f608e91ef4c1650b8f81c4e16d995
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-06-01 17:19:30 +0530

    Added check isMemberOfWorkspace

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -33,6 +33,7 @@ export const isAuthenticated = async (req, res, next) => {
 
     const user = await userRepository.getById(response.id);
     req.user = user.id;
+    
     next();
   } catch (error) {
     console.log('Auth middleware error', error);
--- src/routes/v1/v1Router.js
+++ src/routes/v1/v1Router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import channelRouter from './channel.js';
+import memberRouter from './members.js';
 import userRouter from './users.js';
 import workspaceRouter from './workspaces.js';
 const router = express.Router();
@@ -8,4 +9,5 @@ const router = express.Router();
 router.use('/users', userRouter);
 router.use('/workspaces', workspaceRouter);
 router.use('/channels', channelRouter);
+router.use('/members', memberRouter);
 export default router;
