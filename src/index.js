3e55c015ba8cc3bc2a9e69817b2e59d0b937e235
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-06-10 21:28:39 +0530

    Added socket.io setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
+//import mailer from './config/mailConfig.js';
+import { createServer } from 'http';
 import { StatusCodes } from 'http-status-codes';
+import { Server } from 'socket.io';
 
 import connectDB from './config/dbConfig.js';
-//import mailer from './config/mailConfig.js';
 import { PORT } from './config/serverConfig.js';
 import apiRouter from './routes/apiRoutes.js';
 
 const app = express();
-
+const server = createServer(app);
+const io = new Server(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
@@ -15,7 +18,20 @@ app.get('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'pong' });
 });
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  setTimeout(() => {
+    socket.emit('message', 'this is a message from the server');
+  }, 3000);
+  socket.on('messageFromClient', (data) => {
+    console.log('Message from client', data);
+
+    io.emit('new message', data.toUpperCase());
+  });
+});
+
+server.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 
